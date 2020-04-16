21c01b5c642a41e430d704ed1a4027729598632c
Aravind Koneru <aravind.b.koneru@gmail.com>
2020-04-16 10:37:22 -0400

    changed from buttons to clickable labels for settings page

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
+
 export class Tab5Page implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+      console.log("this");
+  }
+
+  updateProfileClicked() {
+      console.log("update profile");
+  }
+
+  connectWearableClicked() {
+      console.log("connect wearable");
+  }
+
+  manageDataClicked() {
+      console.log("manage data");
+  }
+
+  privacyPolicyClicked() {
+      console.log("privacy policy");
   }
 
 }
